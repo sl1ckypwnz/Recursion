caedbd35761a5e8c16439dfe3339e3b366279dd8
sergey <sl1ckypwnzip@gmail.com>
2024-02-23 14:22:28 +0300

    Добавьте файлы проекта.

---
+++ Recursion.py
@@ -0,0 +1,8 @@
+﻿def f(arr):
+    if not arr:
+        print('Конец списка')
+    else:
+        print(arr[0])
+        f(arr[1:])
+my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+f(my_list)
